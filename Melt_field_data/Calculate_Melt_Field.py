0fe8ffc3065de3f4e7617ac5e52c612cb8b32cf9
Celia Trunz <celia.trunz@gmail.com>
2020-10-08 11:56:27 -0500

    Update Calculate_Melt_Field.py

--- Melt_field_data/Calculate_Melt_Field.py
+++ Melt_field_data/Calculate_Melt_Field.py
@@ -17,6 +17,7 @@ a standard albedo of 0.7 for entire timeseries'''
 
 
 from melt_model_jessica_Oct2020 import weather_data
+
 lowc17= weather_data('LOWC17_WEA_CAT.csv')
 lowc17_melt  = weather_data.calc_melt(lowc17)
 
@@ -31,3 +32,41 @@ lowc18_melt = weather_data.calc_melt(lowc18)
 # 2018 high camp data is missing, use solar rad from low camp
 high18 = weather_data('HIGH18_WEA_CAT.csv')
 high18_melt = high18.calc_melt(incoming_shortwave_radiation=lowc18.solar_corrected)
+
+#%% plot
+
+import matplotlib.pyplot as plt
+
+plt.figure()
+plt.plot(lowc17_melt)
+plt.plot(lowc18_melt)
+plt.plot(high17_melt)
+plt.plot(high18_melt)
+
+
+
+#%%
+#smooth data with a rolling window.
+
+lowc17_melt_rolling = lowc17_melt.rolling(10).mean()
+lowc18_melt_rolling = lowc18_melt.rolling(10).mean()
+high17_melt_rolling = high17_melt.rolling(10).mean()
+high18_melt_rolling = high18_melt.rolling(10).mean()
+
+plt.figure()
+plt.plot(lowc17_melt,color='blue')
+plt.plot(lowc18_melt,color='blue')
+plt.plot(high17_melt,color='blue')
+plt.plot(high18_melt,color='blue')
+plt.plot(lowc17_melt_rolling, color='red')
+plt.plot(lowc18_melt_rolling, color='red')
+plt.plot(high17_melt_rolling, color='red')
+plt.plot(high18_melt_rolling, color='red')
+
+#%%
+#save to csv file
+
+lowc17_melt_rolling.to_csv('lowc17_melt_rolling.csv')
+lowc18_melt_rolling.to_csv('lowc18_melt_rolling.csv')
+high17_melt_rolling.to_csv('high17_melt_rolling.csv')
+high18_melt_rolling.to_csv('high18_melt_rolling.csv')
