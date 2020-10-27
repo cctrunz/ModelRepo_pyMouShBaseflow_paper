f9940f21b92991466fe462d7d46ddc35e8622e1b
Celia Trunz <30932911+cctrunz@users.noreply.github.com>
2020-10-26 22:02:10 -0500

    add figure methods -- in progress

--- PyMouSh.py
+++ PyMouSh.py
@@ -362,7 +362,6 @@ class MoulinShape():
 
         # save values in dictionnary
         self.listdict.append({'index': self.idx,
-                        'time': self.t,
                         'delta_creep_major': self.dC_major,
                         'delta_creep_minor': self.dC_minor,
                         'delta_elastic_major': self.dE_major,
@@ -385,13 +384,184 @@ class MoulinShape():
         self.dict['meltwater_output_subglacial'].append(self.Qout)
         self.dict['head'].append(self.head)
         self.dict['subglacial_cross_section_area'].append(self.subglacial_area)
+        self.time = self.t
 
         #return self.sim
         #update index for nex timestep
         self.idx = self.idx+1
         
+    def plot_head(self,axis,spine_head_min=200,bottom_axis=True):
+        time = self.time/SECINDAY
+        axis.plot(time,self.dict['head'],'-',color='blue') 
         
-    # def plot_agu
+        min_time = int(min(time))
+        max_time = int(max(time))
+        
+        axis.set_ylabel('Head',color='blue')
+        axis.set_xlim([min_time,max_time])
+        axis.set_ylim([-50,self.ice_thickness]) 
+        axis.yaxis.tick_left()
+        axis.yaxis.set_label_position("left")
+        axis.tick_params(axis='y', labelcolor='blue')
+        axis.spines['top'].set_visible(False)
+        
+        axis.spines['right'].set_visible(False)
+        axis.spines['left'].set_color('blue')
+        if bottom_axis == False:
+            axis.spines['bottom'].set_visible(False)
+            axis.axes.xaxis.set_visible(False)
+        axis.spines['left'].set_bounds(spine_head_min,self.ice_thickness)
+        axis.set_yticks(np.arange(spine_head_min,self.ice_thickness+1,100)) 
+        axis.set_yticklabels(np.arange(spine_head_min,self.ice_thickness+1,100))
+  
+        
+    def simple_plot(self):
+        fig = plt.figure(figsize=(25,8))
+        grid = plt.GridSpec(4,4)#, wspace=-0.7)
+        ax1 = fig.add_subplot(grid[0:4, 0])
+        ax2 = fig.add_subplot(grid[0:4, 1:4])#, sharey=ax1)  #hw
+        ax3 = fig.add_subplot(grid[2, 1:4])#ax2.twinx() #SCs
+        ax4 = fig.add_subplot(grid[3, 1:4], sharex=ax2)    #Qin-Qout
+        ax5 = ax4.twinx()#fig1.add_subplot(grid[2, 1:4], sharex=ax2)    #Qin-Qout
+          
+        self.plot_head(ax2)
+    
+    def plot_AGU(self,idx,t_real,h_real,
+              spine_head_min=200
+              ):
+    
+        time = self.time/24/3600
+        z = self.z
+        ice_thickness = self.ice_thickness    
+        head = self.dict['head']
+        subglacial_area = self.dict['subglacial_cross_section_area']   
+        meltwater_input_moulin = self.dict['meltwater_input_moulin']
+        #melwater_input_compensated = moulin.dict['melwater_input_compensated_moulin']
+        meltwater_output_subglacial = self.dict['meltwater_output_subglacial'] 
+        Mx_upstream = self.listdict[idx]['moulin_wall_position_upstream']
+        Mx_downstream = self.listdict[idx]['moulin_wall_position_downstream']        
+          
+        fig = plt.figure(figsize=(25,8))
+        grid = plt.GridSpec(4,4)#, wspace=-0.7)
+        ax1 = fig.add_subplot(grid[0:4, 0])
+        ax2 = fig.add_subplot(grid[0:4, 1:4])#, sharey=ax1)  #hw
+        ax3 = fig.add_subplot(grid[2, 1:4])#ax2.twinx() #SCs
+        ax4 = fig.add_subplot(grid[3, 1:4], sharex=ax2)    #Qin-Qout
+        ax5 = ax4.twinx()#fig1.add_subplot(grid[2, 1:4], sharex=ax2)    #Qin-Qout
+        
+        # # dlim = 10
+        # # dbound = 10
+        # # dtick = [-10,5,0,5,10]
+        
+        time_plot=time[0:idx+1]
+        min_time = int(min(time))
+        max_time = int(max(time))
+        
+        ax1.clear() #clear figure content -- especially important for ploting in the loop
+        ax2.clear()
+        ax3.clear()
+        ax4.clear()
+        ax5.clear() 
+        
+        # #MOULIN SHAPE
+        
+        ax1.plot(Mx_upstream,z,color='black') #plot major axis on the left
+        ax1.plot(Mx_downstream,z,color='black')  #plot minor axis on the right
+        
+        ax2.plot(t_real/3600/24,h_real,'-',color='black')      
+        ax2.plot(time_plot,head[0:idx+1],'-',color='blue')   
+        ax2.legend(['measured','simulated'],loc="upper left")
+         
+        ax3.plot(time_plot,subglacial_area[0:idx+1],'-',color='red')       
+        
+        ax4.plot(time,meltwater_input_moulin,'--',color='blue')#,label='Qin')    
+        ax5.plot(time_plot,meltwater_output_subglacial[0:idx+1],color='red')#,'-',color='red',label='Qout')
+        
+        ax1.axhspan(0, head[idx], facecolor ='lightblue', alpha = 1,zorder=1)
+        ax1.axhspan(-140, 0, facecolor ='peru', alpha = 1,zorder=1)
+        ax1.fill_betweenx(z,-10,Mx_upstream,color='aliceblue',zorder=2)
+        ax1.fill_betweenx(z,Mx_downstream,10,color='aliceblue',zorder=2)     
+        
+        '''Moulin'''
+        ax1.set_ylabel('z(m)')    
+        ax1.set_xlabel('(m)')
+        ax1.set_xlim([-5,5]) 
+        ax1.set_ylim([-50,ice_thickness]) 
+        ax1.spines['top'].set_visible(False)
+        ax1.spines['right'].set_visible(False)
+        ax1.spines['bottom'].set_position(('zero')) 
+        ax1.spines['left'].set_bounds(0,ice_thickness)
+        ax1.spines['bottom'].set_bounds(-5,5)
+        ax1.set_xticks([-4,-3,-2,-1,0,1,2,3,4]) 
+        ax1.set_xticklabels([-4,-3,-2,-1,0,1,2,3,4]) 
+        ax1.set_yticks(np.arange(0,ice_thickness+1,100)) 
+        ax1.set_yticklabels(np.arange(0,ice_thickness+1,100))
+        
+        '''Head'''
+        ax2.set_ylabel('Head',color='blue')
+        ax2.set_xlim([min_time,max_time])
+        ax2.set_ylim([-50,ice_thickness]) 
+        ax2.yaxis.tick_left()
+        ax2.yaxis.set_label_position("left")
+        ax2.tick_params(axis='y', labelcolor='blue')
+        ax2.spines['top'].set_visible(False)
+        ax2.spines['bottom'].set_visible(False)
+        ax2.spines['right'].set_visible(False)
+        ax2.spines['left'].set_color('blue')
+        ax2.axes.xaxis.set_visible(False)
+        ax2.spines['left'].set_bounds(spine_head_min,ice_thickness)
+        ax2.set_yticks(np.arange(spine_head_min,ice_thickness+1,100)) 
+        ax2.set_yticklabels(np.arange(spine_head_min,ice_thickness+1,100))
+        
+        
+        '''Subglacial channel'''
+        ax3.set_ylabel('Subglacial_area',color='red')
+        ax3.set_xlim([min_time,max_time])
+        ax3.set_ylim([min(subglacial_area),max(subglacial_area)])
+        ax3.yaxis.tick_right()
+        ax3.yaxis.set_label_position("right")
+        ax3.tick_params(axis='y', labelcolor='red')
+        ax3.spines['top'].set_visible(False)
+        ax3.spines['bottom'].set_visible(False)
+        ax3.spines['left'].set_visible(False)
+        ax3.spines['right'].set_color('red')
+        ax3.axes.xaxis.set_visible(False)
+        
+        
+        '''Qin'''
+        ax4.set_ylabel('Qin',color='blue')
+        ax4.set_xlim([min_time,max_time])
+        ax4.set_ylim([min(meltwater_input_moulin),max(meltwater_input_moulin)])
+        ax4.spines['top'].set_visible(False)
+        ax4.spines['right'].set_visible(False)
+        ax4.spines['left'].set_color('blue')
+        ax4.tick_params(axis='y', labelcolor='blue')
+        ax4.set_xticks(np.round(np.linspace(min_time,20,max_time)))
+        ax4.set_xticklabels(np.round(np.linspace(min_time,20,max_time)))
+        
+        '''Qout'''
+        ax5.set_ylabel('Qout',color='red')
+        ax5.set_xlim([min_time,max_time])
+        ax5.set_ylim([min(meltwater_output_subglacial),max(meltwater_output_subglacial)])
+        ax5.yaxis.tick_right()
+        ax5.yaxis.set_label_position("right")
+        ax5.tick_params(axis='y', labelcolor='red')
+        ax5.spines['top'].set_visible(False)
+        ax5.spines['left'].set_visible(False)
+        ax5.spines['bottom'].set_visible(False)
+        ax5.spines['right'].set_color('red')
+        
+        
+        
+        #make backgroud transparent    
+        ax1.patch.set_alpha(0)
+        ax2.patch.set_alpha(0)
+        ax3.patch.set_alpha(0)
+        ax4.patch.set_alpha(0)
+        ax5.patch.set_alpha(0)
+            
+        
+    
 
     def calculate_dL(self):  # dL
         """calculates the length of wall for a defined dz"""
@@ -711,129 +881,3 @@ def calculate_h_S_schoof(t, y, moulin_area, z, ice_thickness, overburden_pressur
     return [dhdt, dSdt]
 
 
-# class Plots():
-       
-    
-#     def AGU_plot(self,t):
-        
-#         fig = plt.figure(figsize=(25,8))
-#         grid = plt.GridSpec(4,4)#, wspace=-0.7)
-#         ax1 = fig.add_subplot(grid[0:4, 0])
-#         ax2 = fig.add_subplot(grid[0:4, 1:4])#, sharey=ax1)  #hw
-#         ax3 = fig.add_subplot(grid[2, 1:4])#ax2.twinx() #SCs
-#         ax4 = fig.add_subplot(grid[3, 1:4], sharex=ax2)    #Qin-Qout
-#         ax5 = ax4.twinx()#fig1.add_subplot(grid[2, 1:4], sharex=ax2)    #Qin-Qout
-        
-#         dlim = 10
-#         dbound = 10
-#         dtick = [-10,5,0,5,10]
-    
-#         ax1.clear() #clear figure content -- especially important for ploting in the loop
-#         ax2.clear()
-#         ax3.clear()
-#         ax4.clear()
-#         ax5.clear()
-        
-#         Mx_upstream = results['Mx_upstream']
-#         Mx_downstream = results['Mx_downstream']
-    
-#         hw = results['hw']
-#         SCs = results['SCs']
-#         Qout = results['Qout']
-    
-#         #MOULIN SHAPE
-        
-#         ax1.plot(Mx_upstream[idx],z,color='black') #plot major axis on the left
-#         ax1.plot(Mx_downstream[idx],z,color='black')  #plot minor axis on the right
-        
-#         ax2.plot(t_real/3600/24,h_real,'-',color='black')      
-#         ax2.plot(time[0:idx+1]/3600/24,hw[0:idx+1],'-',color='blue')   
-#         ax2.legend(['measured','simulated'],loc="upper left")
-         
-#         ax3.plot(time[0:idx+1]/3600/24,SCs[0:idx+1],'-',color='red')       
-    
-#         ax4.plot(time/3600/24,Qin,'--',color='blue')#,label='Qin')    
-#         ax5.plot(time[0:idx+1]/3600/24,Qout[0:idx+1],color='red')#,'-',color='red',label='Qout')
-        
-#         ax1.axhspan(0, hw[idx], facecolor ='lightblue', alpha = 1,zorder=1)
-#         ax1.axhspan(-140, 0, facecolor ='peru', alpha = 1,zorder=1)
-#         ax1.fill_betweenx(z,-10,Mx_upstream[idx],color='aliceblue',zorder=2)
-#         ax1.fill_betweenx(z,Mx_downstream[idx],10,color='aliceblue',zorder=2)     
-        
-#         '''Moulin'''
-#         ax1.set_ylabel('z(m)')    
-#         ax1.set_xlabel('(m)')
-#         ax1.set_xlim([-5,5]) 
-#         ax1.set_ylim([-50,H]) 
-#         ax1.spines['top'].set_visible(False)
-#         ax1.spines['right'].set_visible(False)
-#         ax1.spines['bottom'].set_position(('zero')) 
-#         ax1.spines['left'].set_bounds(0,H)
-#         ax1.spines['bottom'].set_bounds(-5,5)
-#         ax1.set_xticks([-4,-3,-2,-1,0,1,2,3,4]) 
-#         ax1.set_xticklabels([-4,-3,-2,-1,0,1,2,3,4]) 
-#         ax1.set_yticks(np.arange(0,H+1,100)) 
-#         ax1.set_yticklabels(np.arange(0,H+1,100))
-    
-#         '''Head'''
-#         ax2.set_ylabel('hw',color='blue')
-#         ax2.set_xlim([0,max(time)/3600/24])
-#         ax2.set_ylim([-50,H]) 
-#         ax2.yaxis.tick_left()
-#         ax2.yaxis.set_label_position("left")
-#         ax2.tick_params(axis='y', labelcolor='blue')
-#         ax2.spines['top'].set_visible(False)
-#         ax2.spines['bottom'].set_visible(False)
-#         ax2.spines['right'].set_visible(False)
-#         ax2.spines['left'].set_color('blue')
-#         ax2.axes.xaxis.set_visible(False)
-#         ax2.spines['left'].set_bounds(hw_min,H)
-#         ax2.set_yticks(np.arange(hw_min,H+1,100)) 
-#         ax2.set_yticklabels(np.arange(hw_min,H+1,100))
-    
-        
-#         '''Subglacial channel'''
-#         ax3.set_ylabel('Scs',color='red')
-#         ax3.set_xlim([0,max(time)/3600/24])
-#         ax3.set_ylim([min(SCs),max(SCs)])
-#         ax3.yaxis.tick_right()
-#         ax3.yaxis.set_label_position("right")
-#         ax3.tick_params(axis='y', labelcolor='red')
-#         ax3.spines['top'].set_visible(False)
-#         ax3.spines['bottom'].set_visible(False)
-#         ax3.spines['left'].set_visible(False)
-#         ax3.spines['right'].set_color('red')
-#         ax3.axes.xaxis.set_visible(False)
-        
-        
-#         '''Qin'''
-#         ax4.set_ylabel('Qin',color='blue')
-#         ax4.set_xlim([0,max(time)/3600/24])
-#         ax4.set_ylim([min(Qin),max(Qin)])
-#         ax4.spines['top'].set_visible(False)
-#         ax4.spines['right'].set_visible(False)
-#         ax4.spines['left'].set_color('blue')
-#         ax4.tick_params(axis='y', labelcolor='blue')
-#         ax4.set_xticks(np.round(np.linspace(1,max(time)/3600/24,20)))
-#         ax4.set_xticklabels(np.round(np.linspace(1,max(time)/3600/24,20)))
-        
-#         '''Qout'''
-#         ax5.set_ylabel('Qout',color='red')
-#         ax5.set_xlim([0,max(time)/3600/24])
-#         ax5.set_ylim([min(Qout),max(Qout)])
-#         ax5.yaxis.tick_right()
-#         ax5.yaxis.set_label_position("right")
-#         ax5.tick_params(axis='y', labelcolor='red')
-#         ax5.spines['top'].set_visible(False)
-#         ax5.spines['left'].set_visible(False)
-#         ax5.spines['bottom'].set_visible(False)
-#         ax5.spines['right'].set_color('red')
-    
-        
-        
-#         #make backgroud transparent    
-#         ax1.patch.set_alpha(0)
-#         ax2.patch.set_alpha(0)
-#         ax3.patch.set_alpha(0)
-#         ax4.patch.set_alpha(0)
-#         ax5.patch.set_alpha(0)
--- run_class.py
+++ run_class.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+#%%
 secinday = 24*3600
 ZERO_KELVIN = 273.15
 
@@ -77,9 +78,9 @@ for t in time :
    
 #moulin.listdict[0].keys()    
 #moulin.dict.keys()
-
-
-
+#%%
+idx = 10000
+moulin.plot_AGU(idx,t_real,h_real,spine_head_min=200)
 #%%
 
 # plt.figure()
@@ -96,143 +97,143 @@ for t in time :
 #     plt.pause(0.1)
 #%%
 
-def plot_AGU(moulin, 
-              time,
-              idx,
-              t_real,
-              h_real,
-              spine_head_min=200
-              ):
+# def plot_AGU(moulin, 
+#               time,
+#               idx,
+#               t_real,
+#               h_real,
+#               spine_head_min=200
+#               ):
 
-    time = time/24/3600
-    z = moulin.z
-    ice_thickness = moulin.ice_thickness    
-    head = moulin.dict['head']
-    subglacial_area = moulin.dict['subglacial_cross_section_area']   
-    meltwater_input_moulin = moulin.dict['meltwater_input_moulin']
-    #melwater_input_compensated = moulin.dict['melwater_input_compensated_moulin']
-    meltwater_output_subglacial = moulin.dict['meltwater_output_subglacial'] 
-    Mx_upstream = moulin.listdict[idx]['moulin_wall_position_upstream']
-    Mx_downstream = moulin.listdict[idx]['moulin_wall_position_downstream']        
+#     time = time/24/3600
+#     z = moulin.z
+#     ice_thickness = moulin.ice_thickness    
+#     head = moulin.dict['head']
+#     subglacial_area = moulin.dict['subglacial_cross_section_area']   
+#     meltwater_input_moulin = moulin.dict['meltwater_input_moulin']
+#     #melwater_input_compensated = moulin.dict['melwater_input_compensated_moulin']
+#     meltwater_output_subglacial = moulin.dict['meltwater_output_subglacial'] 
+#     Mx_upstream = moulin.listdict[idx]['moulin_wall_position_upstream']
+#     Mx_downstream = moulin.listdict[idx]['moulin_wall_position_downstream']        
       
-    fig = plt.figure(figsize=(25,8))
-    grid = plt.GridSpec(4,4)#, wspace=-0.7)
-    ax1 = fig.add_subplot(grid[0:4, 0])
-    ax2 = fig.add_subplot(grid[0:4, 1:4])#, sharey=ax1)  #hw
-    ax3 = fig.add_subplot(grid[2, 1:4])#ax2.twinx() #SCs
-    ax4 = fig.add_subplot(grid[3, 1:4], sharex=ax2)    #Qin-Qout
-    ax5 = ax4.twinx()#fig1.add_subplot(grid[2, 1:4], sharex=ax2)    #Qin-Qout
+#     fig = plt.figure(figsize=(25,8))
+#     grid = plt.GridSpec(4,4)#, wspace=-0.7)
+#     ax1 = fig.add_subplot(grid[0:4, 0])
+#     ax2 = fig.add_subplot(grid[0:4, 1:4])#, sharey=ax1)  #hw
+#     ax3 = fig.add_subplot(grid[2, 1:4])#ax2.twinx() #SCs
+#     ax4 = fig.add_subplot(grid[3, 1:4], sharex=ax2)    #Qin-Qout
+#     ax5 = ax4.twinx()#fig1.add_subplot(grid[2, 1:4], sharex=ax2)    #Qin-Qout
     
-    # # dlim = 10
-    # # dbound = 10
-    # # dtick = [-10,5,0,5,10]
+#     # # dlim = 10
+#     # # dbound = 10
+#     # # dtick = [-10,5,0,5,10]
     
-    time_plot=time[0:idx+1]
-    min_time = int(min(time))
-    max_time = int(max(time))
+#     time_plot=time[0:idx+1]
+#     min_time = int(min(time))
+#     max_time = int(max(time))
     
-    ax1.clear() #clear figure content -- especially important for ploting in the loop
-    ax2.clear()
-    ax3.clear()
-    ax4.clear()
-    ax5.clear() 
+#     ax1.clear() #clear figure content -- especially important for ploting in the loop
+#     ax2.clear()
+#     ax3.clear()
+#     ax4.clear()
+#     ax5.clear() 
     
-    # #MOULIN SHAPE
+#     # #MOULIN SHAPE
     
-    ax1.plot(Mx_upstream,z,color='black') #plot major axis on the left
-    ax1.plot(Mx_downstream,z,color='black')  #plot minor axis on the right
+#     ax1.plot(Mx_upstream,z,color='black') #plot major axis on the left
+#     ax1.plot(Mx_downstream,z,color='black')  #plot minor axis on the right
     
-    ax2.plot(t_real/3600/24,h_real,'-',color='black')      
-    ax2.plot(time_plot,head[0:idx+1],'-',color='blue')   
-    ax2.legend(['measured','simulated'],loc="upper left")
+#     ax2.plot(t_real/3600/24,h_real,'-',color='black')      
+#     ax2.plot(time_plot,head[0:idx+1],'-',color='blue')   
+#     ax2.legend(['measured','simulated'],loc="upper left")
      
-    ax3.plot(time_plot,subglacial_area[0:idx+1],'-',color='red')       
+#     ax3.plot(time_plot,subglacial_area[0:idx+1],'-',color='red')       
     
-    ax4.plot(time,meltwater_input_moulin,'--',color='blue')#,label='Qin')    
-    ax5.plot(time_plot,meltwater_output_subglacial[0:idx+1],color='red')#,'-',color='red',label='Qout')
+#     ax4.plot(time,meltwater_input_moulin,'--',color='blue')#,label='Qin')    
+#     ax5.plot(time_plot,meltwater_output_subglacial[0:idx+1],color='red')#,'-',color='red',label='Qout')
     
-    ax1.axhspan(0, head[idx], facecolor ='lightblue', alpha = 1,zorder=1)
-    ax1.axhspan(-140, 0, facecolor ='peru', alpha = 1,zorder=1)
-    ax1.fill_betweenx(z,-10,Mx_upstream,color='aliceblue',zorder=2)
-    ax1.fill_betweenx(z,Mx_downstream,10,color='aliceblue',zorder=2)     
+#     ax1.axhspan(0, head[idx], facecolor ='lightblue', alpha = 1,zorder=1)
+#     ax1.axhspan(-140, 0, facecolor ='peru', alpha = 1,zorder=1)
+#     ax1.fill_betweenx(z,-10,Mx_upstream,color='aliceblue',zorder=2)
+#     ax1.fill_betweenx(z,Mx_downstream,10,color='aliceblue',zorder=2)     
     
-    '''Moulin'''
-    ax1.set_ylabel('z(m)')    
-    ax1.set_xlabel('(m)')
-    ax1.set_xlim([-5,5]) 
-    ax1.set_ylim([-50,ice_thickness]) 
-    ax1.spines['top'].set_visible(False)
-    ax1.spines['right'].set_visible(False)
-    ax1.spines['bottom'].set_position(('zero')) 
-    ax1.spines['left'].set_bounds(0,ice_thickness)
-    ax1.spines['bottom'].set_bounds(-5,5)
-    ax1.set_xticks([-4,-3,-2,-1,0,1,2,3,4]) 
-    ax1.set_xticklabels([-4,-3,-2,-1,0,1,2,3,4]) 
-    ax1.set_yticks(np.arange(0,ice_thickness+1,100)) 
-    ax1.set_yticklabels(np.arange(0,ice_thickness+1,100))
+#     '''Moulin'''
+#     ax1.set_ylabel('z(m)')    
+#     ax1.set_xlabel('(m)')
+#     ax1.set_xlim([-5,5]) 
+#     ax1.set_ylim([-50,ice_thickness]) 
+#     ax1.spines['top'].set_visible(False)
+#     ax1.spines['right'].set_visible(False)
+#     ax1.spines['bottom'].set_position(('zero')) 
+#     ax1.spines['left'].set_bounds(0,ice_thickness)
+#     ax1.spines['bottom'].set_bounds(-5,5)
+#     ax1.set_xticks([-4,-3,-2,-1,0,1,2,3,4]) 
+#     ax1.set_xticklabels([-4,-3,-2,-1,0,1,2,3,4]) 
+#     ax1.set_yticks(np.arange(0,ice_thickness+1,100)) 
+#     ax1.set_yticklabels(np.arange(0,ice_thickness+1,100))
     
-    '''Head'''
-    ax2.set_ylabel('Head',color='blue')
-    ax2.set_xlim([min_time,max_time])
-    ax2.set_ylim([-50,ice_thickness]) 
-    ax2.yaxis.tick_left()
-    ax2.yaxis.set_label_position("left")
-    ax2.tick_params(axis='y', labelcolor='blue')
-    ax2.spines['top'].set_visible(False)
-    ax2.spines['bottom'].set_visible(False)
-    ax2.spines['right'].set_visible(False)
-    ax2.spines['left'].set_color('blue')
-    ax2.axes.xaxis.set_visible(False)
-    ax2.spines['left'].set_bounds(spine_head_min,ice_thickness)
-    ax2.set_yticks(np.arange(spine_head_min,ice_thickness+1,100)) 
-    ax2.set_yticklabels(np.arange(spine_head_min,ice_thickness+1,100))
-    
-    
-    '''Subglacial channel'''
-    ax3.set_ylabel('Subglacial_area',color='red')
-    ax3.set_xlim([min_time,max_time])
-    ax3.set_ylim([min(subglacial_area),max(subglacial_area)])
-    ax3.yaxis.tick_right()
-    ax3.yaxis.set_label_position("right")
-    ax3.tick_params(axis='y', labelcolor='red')
-    ax3.spines['top'].set_visible(False)
-    ax3.spines['bottom'].set_visible(False)
-    ax3.spines['left'].set_visible(False)
-    ax3.spines['right'].set_color('red')
-    ax3.axes.xaxis.set_visible(False)
+#     '''Head'''
+#     ax2.set_ylabel('Head',color='blue')
+#     ax2.set_xlim([min_time,max_time])
+#     ax2.set_ylim([-50,ice_thickness]) 
+#     ax2.yaxis.tick_left()
+#     ax2.yaxis.set_label_position("left")
+#     ax2.tick_params(axis='y', labelcolor='blue')
+#     ax2.spines['top'].set_visible(False)
+#     ax2.spines['bottom'].set_visible(False)
+#     ax2.spines['right'].set_visible(False)
+#     ax2.spines['left'].set_color('blue')
+#     ax2.axes.xaxis.set_visible(False)
+#     ax2.spines['left'].set_bounds(spine_head_min,ice_thickness)
+#     ax2.set_yticks(np.arange(spine_head_min,ice_thickness+1,100)) 
+#     ax2.set_yticklabels(np.arange(spine_head_min,ice_thickness+1,100))
     
     
-    '''Qin'''
-    ax4.set_ylabel('Qin',color='blue')
-    ax4.set_xlim([min_time,max_time])
-    ax4.set_ylim([min(meltwater_input_moulin),max(meltwater_input_moulin)])
-    ax4.spines['top'].set_visible(False)
-    ax4.spines['right'].set_visible(False)
-    ax4.spines['left'].set_color('blue')
-    ax4.tick_params(axis='y', labelcolor='blue')
-    ax4.set_xticks(np.round(np.linspace(min_time,20,max_time)))
-    ax4.set_xticklabels(np.round(np.linspace(min_time,20,max_time)))
-    
-    '''Qout'''
-    ax5.set_ylabel('Qout',color='red')
-    ax5.set_xlim([min_time,max_time])
-    ax5.set_ylim([min(meltwater_output_subglacial),max(meltwater_output_subglacial)])
-    ax5.yaxis.tick_right()
-    ax5.yaxis.set_label_position("right")
-    ax5.tick_params(axis='y', labelcolor='red')
-    ax5.spines['top'].set_visible(False)
-    ax5.spines['left'].set_visible(False)
-    ax5.spines['bottom'].set_visible(False)
-    ax5.spines['right'].set_color('red')
+#     '''Subglacial channel'''
+#     ax3.set_ylabel('Subglacial_area',color='red')
+#     ax3.set_xlim([min_time,max_time])
+#     ax3.set_ylim([min(subglacial_area),max(subglacial_area)])
+#     ax3.yaxis.tick_right()
+#     ax3.yaxis.set_label_position("right")
+#     ax3.tick_params(axis='y', labelcolor='red')
+#     ax3.spines['top'].set_visible(False)
+#     ax3.spines['bottom'].set_visible(False)
+#     ax3.spines['left'].set_visible(False)
+#     ax3.spines['right'].set_color('red')
+#     ax3.axes.xaxis.set_visible(False)
     
     
+#     '''Qin'''
+#     ax4.set_ylabel('Qin',color='blue')
+#     ax4.set_xlim([min_time,max_time])
+#     ax4.set_ylim([min(meltwater_input_moulin),max(meltwater_input_moulin)])
+#     ax4.spines['top'].set_visible(False)
+#     ax4.spines['right'].set_visible(False)
+#     ax4.spines['left'].set_color('blue')
+#     ax4.tick_params(axis='y', labelcolor='blue')
+#     ax4.set_xticks(np.round(np.linspace(min_time,20,max_time)))
+#     ax4.set_xticklabels(np.round(np.linspace(min_time,20,max_time)))
     
-    #make backgroud transparent    
-    ax1.patch.set_alpha(0)
-    ax2.patch.set_alpha(0)
-    ax3.patch.set_alpha(0)
-    ax4.patch.set_alpha(0)
-    ax5.patch.set_alpha(0)
+#     '''Qout'''
+#     ax5.set_ylabel('Qout',color='red')
+#     ax5.set_xlim([min_time,max_time])
+#     ax5.set_ylim([min(meltwater_output_subglacial),max(meltwater_output_subglacial)])
+#     ax5.yaxis.tick_right()
+#     ax5.yaxis.set_label_position("right")
+#     ax5.tick_params(axis='y', labelcolor='red')
+#     ax5.spines['top'].set_visible(False)
+#     ax5.spines['left'].set_visible(False)
+#     ax5.spines['bottom'].set_visible(False)
+#     ax5.spines['right'].set_color('red')
+    
+    
+    
+#     #make backgroud transparent    
+#     ax1.patch.set_alpha(0)
+#     ax2.patch.set_alpha(0)
+#     ax3.patch.set_alpha(0)
+#     ax4.patch.set_alpha(0)
+#     ax5.patch.set_alpha(0)
      
 #%%
 # idx = 3000
