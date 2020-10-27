3ce77017ef1c029bc1ef9ed1c921602f2b83c871
Celia Trunz <celia.trunz@gmail.com>
2020-10-26 19:44:07 -0500

    update list and dict type in pymoush

--- PyMouSh.py
+++ PyMouSh.py
@@ -13,9 +13,10 @@ Component provenance:
 import numpy as np
 
 #from typing import Union, Tuple#, ArrayLike
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 from scipy.integrate import solve_ivp
 from scipy.integrate import cumtrapz
+from collections import defaultdict
 #import pandas as pd
 #import plot_codes.comprehensive_plot
 
@@ -178,7 +179,8 @@ class MoulinShape():
         self.dGlen = 0
         self.dGlen_cumulative = 0
         self.idx = 0
-        self.sim = []
+        self.listdict = []
+        self.dict = defaultdict(list)
 
 
         #Initialize moulin radius
@@ -224,6 +226,8 @@ class MoulinShape():
                  ice_motion=True,
                  refreezing=False):
         
+
+        
         self.include_ice_temperature = include_ice_temperature
         self.overflow = overflow
         self.creep = creep
@@ -240,6 +244,8 @@ class MoulinShape():
         self.Qin = meltwater_input_timeserie[self.idx]
         #calculate lenght of current timestep (s)
         self.dt = dt#self.t[self.idx+1]-self.t[self.idx]
+        
+
 
         # moulin geometry properties calculated for each time step
         ellipse_perimeter = np.pi * (3 * (self.Mr_minor + self.Mr_major) - np.sqrt(
@@ -279,7 +285,7 @@ class MoulinShape():
         ##################
 
         # calculate meltwater discharge out of the subglacial channel
-        self.Qout = self.C3*self.subglacial_area**(5/4)*np.sqrt(
+        self.Qout= self.C3*self.subglacial_area**(5/4)*np.sqrt(
             WATER_DENSITY*GRAVITY*self.head/self.channel_length)
         # locate z nodes with at and under the water level
         self.wet = self.z <= self.head
@@ -351,18 +357,12 @@ class MoulinShape():
         self.Mx_upstream = self.Mx_upstream + self.dGlen - self.dr_major
         self.Mx_downstream = self.Mx_downstream + self.dGlen + self.dr_minor
         
-        #update index for next timestep
-        self.idx = self.idx+1
+
         
 
         # save values in dictionnary
-        self.sim.append({'index': self.idx,
+        self.listdict.append({'index': self.idx,
                         'time': self.t,
-                        'meltwater_input_moulin': self.Qin,
-                        'melwater_input_compensated_moulin': self.Qin_compensated,
-                        'meltwater_output_subglacial': self.Qout,
-                        'head': self.head,
-                        'subglacial_cross_section_area': self.subglacial_area,
                         'delta_creep_major': self.dC_major,
                         'delta_creep_minor': self.dC_minor,
                         'delta_elastic_major': self.dE_major,
@@ -379,8 +379,19 @@ class MoulinShape():
                         'moulin_wall_position_upstream': self.Mx_upstream,
                         'moulin_wall_position_downstream': self.Mx_downstream
                         })
-        
+
+        self.dict['meltwater_input_moulin'].append(self.Qin)
+        self.dict['melwater_input_compensated_moulin'].append(self.Qin_compensated)
+        self.dict['meltwater_output_subglacial'].append(self.Qout)
+        self.dict['head'].append(self.head)
+        self.dict['subglacial_cross_section_area'].append(self.subglacial_area)
+
         #return self.sim
+        #update index for nex timestep
+        self.idx = self.idx+1
+        
+        
+    # def plot_agu
 
     def calculate_dL(self):  # dL
         """calculates the length of wall for a defined dz"""
@@ -586,6 +597,35 @@ class MoulinShape():
         dF[~self.wet] = 0
         return dF
     
+    
+    
+            
+ 
+    
+   
+
+
+
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 #create time array for run
 def TimeStamps(time_start, time_end, timestep):     
     return np.arange(time_start, time_end+1, timestep)
@@ -669,3 +709,131 @@ def calculate_h_S_schoof(t, y, moulin_area, z, ice_thickness, overburden_pressur
                 dhdt = 0
 
     return [dhdt, dSdt]
+
+
+# class Plots():
+       
+    
+#     def AGU_plot(self,t):
+        
+#         fig = plt.figure(figsize=(25,8))
+#         grid = plt.GridSpec(4,4)#, wspace=-0.7)
+#         ax1 = fig.add_subplot(grid[0:4, 0])
+#         ax2 = fig.add_subplot(grid[0:4, 1:4])#, sharey=ax1)  #hw
+#         ax3 = fig.add_subplot(grid[2, 1:4])#ax2.twinx() #SCs
+#         ax4 = fig.add_subplot(grid[3, 1:4], sharex=ax2)    #Qin-Qout
+#         ax5 = ax4.twinx()#fig1.add_subplot(grid[2, 1:4], sharex=ax2)    #Qin-Qout
+        
+#         dlim = 10
+#         dbound = 10
+#         dtick = [-10,5,0,5,10]
+    
+#         ax1.clear() #clear figure content -- especially important for ploting in the loop
+#         ax2.clear()
+#         ax3.clear()
+#         ax4.clear()
+#         ax5.clear()
+        
+#         Mx_upstream = results['Mx_upstream']
+#         Mx_downstream = results['Mx_downstream']
+    
+#         hw = results['hw']
+#         SCs = results['SCs']
+#         Qout = results['Qout']
+    
+#         #MOULIN SHAPE
+        
+#         ax1.plot(Mx_upstream[idx],z,color='black') #plot major axis on the left
+#         ax1.plot(Mx_downstream[idx],z,color='black')  #plot minor axis on the right
+        
+#         ax2.plot(t_real/3600/24,h_real,'-',color='black')      
+#         ax2.plot(time[0:idx+1]/3600/24,hw[0:idx+1],'-',color='blue')   
+#         ax2.legend(['measured','simulated'],loc="upper left")
+         
+#         ax3.plot(time[0:idx+1]/3600/24,SCs[0:idx+1],'-',color='red')       
+    
+#         ax4.plot(time/3600/24,Qin,'--',color='blue')#,label='Qin')    
+#         ax5.plot(time[0:idx+1]/3600/24,Qout[0:idx+1],color='red')#,'-',color='red',label='Qout')
+        
+#         ax1.axhspan(0, hw[idx], facecolor ='lightblue', alpha = 1,zorder=1)
+#         ax1.axhspan(-140, 0, facecolor ='peru', alpha = 1,zorder=1)
+#         ax1.fill_betweenx(z,-10,Mx_upstream[idx],color='aliceblue',zorder=2)
+#         ax1.fill_betweenx(z,Mx_downstream[idx],10,color='aliceblue',zorder=2)     
+        
+#         '''Moulin'''
+#         ax1.set_ylabel('z(m)')    
+#         ax1.set_xlabel('(m)')
+#         ax1.set_xlim([-5,5]) 
+#         ax1.set_ylim([-50,H]) 
+#         ax1.spines['top'].set_visible(False)
+#         ax1.spines['right'].set_visible(False)
+#         ax1.spines['bottom'].set_position(('zero')) 
+#         ax1.spines['left'].set_bounds(0,H)
+#         ax1.spines['bottom'].set_bounds(-5,5)
+#         ax1.set_xticks([-4,-3,-2,-1,0,1,2,3,4]) 
+#         ax1.set_xticklabels([-4,-3,-2,-1,0,1,2,3,4]) 
+#         ax1.set_yticks(np.arange(0,H+1,100)) 
+#         ax1.set_yticklabels(np.arange(0,H+1,100))
+    
+#         '''Head'''
+#         ax2.set_ylabel('hw',color='blue')
+#         ax2.set_xlim([0,max(time)/3600/24])
+#         ax2.set_ylim([-50,H]) 
+#         ax2.yaxis.tick_left()
+#         ax2.yaxis.set_label_position("left")
+#         ax2.tick_params(axis='y', labelcolor='blue')
+#         ax2.spines['top'].set_visible(False)
+#         ax2.spines['bottom'].set_visible(False)
+#         ax2.spines['right'].set_visible(False)
+#         ax2.spines['left'].set_color('blue')
+#         ax2.axes.xaxis.set_visible(False)
+#         ax2.spines['left'].set_bounds(hw_min,H)
+#         ax2.set_yticks(np.arange(hw_min,H+1,100)) 
+#         ax2.set_yticklabels(np.arange(hw_min,H+1,100))
+    
+        
+#         '''Subglacial channel'''
+#         ax3.set_ylabel('Scs',color='red')
+#         ax3.set_xlim([0,max(time)/3600/24])
+#         ax3.set_ylim([min(SCs),max(SCs)])
+#         ax3.yaxis.tick_right()
+#         ax3.yaxis.set_label_position("right")
+#         ax3.tick_params(axis='y', labelcolor='red')
+#         ax3.spines['top'].set_visible(False)
+#         ax3.spines['bottom'].set_visible(False)
+#         ax3.spines['left'].set_visible(False)
+#         ax3.spines['right'].set_color('red')
+#         ax3.axes.xaxis.set_visible(False)
+        
+        
+#         '''Qin'''
+#         ax4.set_ylabel('Qin',color='blue')
+#         ax4.set_xlim([0,max(time)/3600/24])
+#         ax4.set_ylim([min(Qin),max(Qin)])
+#         ax4.spines['top'].set_visible(False)
+#         ax4.spines['right'].set_visible(False)
+#         ax4.spines['left'].set_color('blue')
+#         ax4.tick_params(axis='y', labelcolor='blue')
+#         ax4.set_xticks(np.round(np.linspace(1,max(time)/3600/24,20)))
+#         ax4.set_xticklabels(np.round(np.linspace(1,max(time)/3600/24,20)))
+        
+#         '''Qout'''
+#         ax5.set_ylabel('Qout',color='red')
+#         ax5.set_xlim([0,max(time)/3600/24])
+#         ax5.set_ylim([min(Qout),max(Qout)])
+#         ax5.yaxis.tick_right()
+#         ax5.yaxis.set_label_position("right")
+#         ax5.tick_params(axis='y', labelcolor='red')
+#         ax5.spines['top'].set_visible(False)
+#         ax5.spines['left'].set_visible(False)
+#         ax5.spines['bottom'].set_visible(False)
+#         ax5.spines['right'].set_color('red')
+    
+        
+        
+#         #make backgroud transparent    
+#         ax1.patch.set_alpha(0)
+#         ax2.patch.set_alpha(0)
+#         ax3.patch.set_alpha(0)
+#         ax4.patch.set_alpha(0)
+#         ax5.patch.set_alpha(0)
