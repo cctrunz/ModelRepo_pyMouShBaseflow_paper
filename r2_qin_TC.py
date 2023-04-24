61355ee6a4335648ce50d70002e33e8af0a665b7
Trunz <trunzc@oregonstate.edu>
2023-04-24 12:35:59 -0700

    unknown change broke plot-figure-jeme code

--- r2_qin_TC.py
+++ r2_qin_TC.py
@@ -26,6 +26,102 @@ model_raw = pd.read_csv('Field_Data/JEME_QSUH.csv', index_col=0, parse_dates=Tru
 
 # all on the same timestep
 dye = dye_raw.Q.groupby(pd.Grouper(freq='1H')).mean()
+#stage = stage_raw.stage.groupby(pd.Grouper(freq='1H')).mean()
+
+#
+#stage_long = stage[(stage.index > stage.index[0]) & (stage.index < stage.index[-1])]  
+#model_long = model_raw.Q[(model_raw.index > dye.index[0]) & (model_raw.index < dye.index[-1])]       
+
+# same period of time
+#stage = stage[(stage.index > dye.index[0]) & (stage.index < dye.index[-1])]  
+model = model_raw.Q[(model_raw.index > dye.index[0]) & (model_raw.index < dye.index[-1])]     
+
+#add missing timesteps and fill missing days without data with nans to obtain homogenous data
+#idx_long = pd.date_range(stage_long.index[0], stage_long.index[-1], freq='1H')
+#stage_long = stage_long.reindex(idx_long, fill_value=np.NaN)
+#model_long = model_long.reindex(idx_long, fill_value=np.NaN)
+
+#add missing timesteps and fill missing days without data with nans to obtain homogenous data
+idx = pd.date_range(dye.index[0], dye.index[-1], freq='1H')
+#stage = stage.reindex(idx, fill_value=np.NaN)
+dye = dye.reindex(idx, fill_value=np.NaN)
+model = model.reindex(idx, fill_value=np.NaN)
+
+#%% Comparison with dye
+date_mid = '2017-07-24 07:00:00'
+# varx = dye.values
+# varx_a = dye[dye.index[0]:date_mid].values
+# varx_b = dye[date_mid:dye.index[-1]].values
+# varys =  stage.values
+# varys_a =  stage[dye.index[0]:date_mid].values
+# varys_b =  stage[date_mid:dye.index[-1]].values
+# varym =  model.values
+# varym_a =  model[dye.index[0]:date_mid].values
+# varym_b =  model[date_mid:dye.index[-1]].values
+
+# varx = stage.values  
+# varx_a = stage[dye.index[0]:date_mid].values
+# varx_b = stage[date_mid:dye.index[-1]].values
+
+varx =  dye[1:-2].values
+# varys_a =   dye[dye.index[0]:date_mid].values
+# varys_b =   dye[date_mid:dye.index[-1]].values
+
+vary =  model[1:-2].values
+# varym_a =  model[dye.index[0]:date_mid].values
+# varym_b =  model[date_mid:dye.index[-1]].values
+
+# mask_stage = ~np.isnan(varx) & ~np.isnan(varys)
+# mask_stage_a = ~np.isnan(varx_a) & ~np.isnan(varys_a)
+# mask_stage_b = ~np.isnan(varx_b) & ~np.isnan(varys_b)
+
+# mask_model = ~np.isnan(varx) & ~np.isnan(varym)
+# mask_model_a = ~np.isnan(varx_a) & ~np.isnan(varym_a)
+# mask_model_b = ~np.isnan(varx_b) & ~np.isnan(varym_b)
+
+# slope_stage, intercept_stage, r_value_stage, p_value_stage, std_err_stage = stats.linregress(varx[mask_stage], varys[mask_stage])
+# slope_stage_a, intercept_stage_a, r_value_stage_a, p_value_stage_a, std_err_stage_a = stats.linregress(varx_a[mask_stage_a], varys_a[mask_stage_a])
+# slope_stage_b, intercept_stage_b, r_value_stage_b, p_value_stage_b, std_err_stage_b = stats.linregress(varx_b[mask_stage_b], varys_b[mask_stage_b])
+
+slope, intercept, r_value, p_value, std_err = stats.linregress(varx, vary)
+# slope_model_a, intercept_model_a, r_value_model_a, p_value_model_a, std_err_model_a = stats.linregress(varx_a[mask_model_a], varym_a[mask_model_a])
+# slope_model_b, intercept_model_b, r_value_model_b, p_value_model_b, std_err_model_b = stats.linregress(varx_b[mask_model_b], varym_b[mask_model_b])
+
+import math
+
+
+
+RMSE = math.sqrt(np.square(np.subtract(vary,varx)).mean())
+
+print("Root Mean Square Error:\n")
+print(RMSE)
+
+
+#%%
+
+n = len(dye)
+color=cm.rainbow(np.linspace(0,1,n))
+
+plt.figure()
+for i,c in zip(range(n),color):
+    plt.plot(dye.values[i],model.values[i],marker='o', linestyle='', c=c)
+    plt.plot(varx,varx*slope+intercept,color='grey')
+
+#plt.plot(dye.values,stage.values,marker='o', linestyle='')
+plt.text(2.85,0.3,'All (grey): R$^2$=%s, p=%s\nDay1'
+         %(round(r_value**2,2),p_value
+           ))
+plt.xlabel('Discharge (dye)')
+plt.ylabel('Discharge (model)')
+
+
+
+#%%
+#%%
+#%%
+
+# all on the same timestep
+dye = dye_raw.Q.groupby(pd.Grouper(freq='1H')).mean()
 stage = stage_raw.stage.groupby(pd.Grouper(freq='1H')).mean()
 
 #
@@ -135,7 +231,7 @@ plt.ylabel('Discharge (model)')
 
 
 
-# #%% Comparison with stream stage
+# #%% Comparison dye vs model
 
 # varx = dye.values
 
